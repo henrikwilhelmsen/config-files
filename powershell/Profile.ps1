42cf5e8a13005f2b4957d8119cafde2625454ffd
Henrik Wilhelmsen <contact@henrikwilhelmsen.com>
2024-02-15 23:42:49 +0100

    update omp theme, remove commented code

--- powershell/Profile.ps1
+++ powershell/Profile.ps1
@@ -1,5 +1,5 @@
-# Init oh-my-posh
-oh-my-posh --init --shell pwsh --config "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/ys.omp.json" | Invoke-Expression
+# Init oh-my-posh, themes: ys, material
+oh-my-posh --init --shell pwsh --config "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/material.omp.json" | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 Import-Module PSReadLine
@@ -8,9 +8,4 @@ Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
 
-# Make sure pyenv is on path
-# $Env:path = $env:USERPROFILE + "\.pyenv\pyenv-win\bin;" + $env:USERPROFILE + "\.pyenv\pyenv-win\shims;" + $env:path
-
-# python and pipenv
 $Env:PYTHONDONTWRITEBYTECODE = 1
-$Env:VIRTUALENV_ALWAYS_COPY = 1
